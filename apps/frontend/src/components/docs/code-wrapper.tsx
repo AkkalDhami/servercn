b49b28e68f98cbd59da13c8dfa641bfe81233196
AkkalDhami <dhamiakkal21@gmail.com>
2026-01-06 06:26:12 +0545

    Refactor CodeWrapper and CopyButton components for improved readability and maintainability, including minor adjustments to class names and layout properties.

--- apps/frontend/src/components/docs/code-wrapper.tsx
+++ apps/frontend/src/components/docs/code-wrapper.tsx
@@ -24,7 +24,7 @@ export function CodeWrapper({
       <CopyButton
         handleCopy={copy}
         copied={copied}
-        className="absolute cursor-pointer right-2 top-1/2 -translate-y-1/2 z-10 bg-[#0b0e14] text-xs text-neutral-400 hover:text-white"
+        className="absolute top-1/2 right-2 z-10 -translate-y-1/2 cursor-pointer bg-[#0b0e14] text-xs text-neutral-400 hover:text-white"
       />
       {children}
     </div>
--- apps/frontend/src/components/docs/copy-button.tsx
+++ apps/frontend/src/components/docs/copy-button.tsx
@@ -14,29 +14,28 @@ export default function CopyButton({
     <button
       aria-label={copied ? "Copied" : "Copy to clipboard"}
       className={cn(
-        "absolute cursor-pointer top-1/2 right-0 -translate-y-1/2 flex w-9 items-center justify-center rounded-e-md text-muted-secondary outline-none transition-[color,box-shadow] hover:text-foreground focus:z-10 focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:cursor-not-allowed",
+        "text-muted-secondary hover:text-foreground focus-visible:border-ring focus-visible:ring-ring/50 absolute top-1/2 right-0 flex w-9 -translate-y-1/2 cursor-pointer items-center justify-center rounded-e-md transition-[color,box-shadow] outline-none focus:z-10 focus-visible:ring-[3px] disabled:pointer-events-none disabled:cursor-not-allowed",
         "duration-100 ease-in-out",
-        className
+        className,
       )}
       disabled={copied}
       onClick={handleCopy}
-      type="button">
+      type="button"
+    >
       <div
         className={cn(
           "transition-all",
-          copied ? "scale-100 opacity-100" : "scale-0 opacity-0"
-        )}>
-        <CheckIcon
-          aria-hidden="true"
-          className="stroke-current"
-          size={16}
-        />
+          copied ? "scale-100 opacity-100" : "scale-0 opacity-0",
+        )}
+      >
+        <CheckIcon aria-hidden="true" className="stroke-current" size={16} />
       </div>
       <div
         className={cn(
           "absolute transition-all",
-          copied ? "scale-0 opacity-0" : "scale-100 opacity-100"
-        )}>
+          copied ? "scale-0 opacity-0" : "scale-100 opacity-100",
+        )}
+      >
         <CopyIcon aria-hidden="true" size={16} />
       </div>
     </button>
