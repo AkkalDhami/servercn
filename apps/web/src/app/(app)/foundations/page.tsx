b73c3b0f72b025eb3c2220dfc97165e16f6193cc
AkkalDhami <dhamiakkal21@gmail.com>
2026-01-08 09:03:50 +0545

    Add FoundationsPage component: Create a new page to display ServerCN foundations, including metadata generation, component listing, and version details for improved documentation and user navigation.

---
+++ apps/web/src/app/(app)/foundations/page.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import { Container } from "@/components/ui/container";
+
+import registry from "@/data/registry.json";
+import { SubHeading } from "@/components/ui/sub-heading";
+import { Heading } from "@/components/ui/heading";
+import ComponentCard from "@/components/docs/component-card";
+import { BackendComponent } from "../components/page";
+
+export const generateMetadata = (): Metadata => {
+  return {
+    title: "Foundations",
+  };
+};
+
+
+const foundations = registry.items
+  .filter((component) => component.type === "foundation")
+  .sort((a, b) => a.title.localeCompare(b.title)) as BackendComponent[];
+
+export default function FoundationsPage() {
+  return (
+    <Container className="mt-16 min-h-screen w-full max-w-360">
+      <div className="mb-6">
+        <Heading className="tracking-tight">ServerCN Foundations</Heading>
+        <SubHeading className="text-muted-foreground mx-0 mt-2">
+          Production-ready ServerCN foundations for building scalable backends.
+          Here you can find all the components available in the library. We are
+          working on adding more components.
+        </SubHeading>
+      </div>
+
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+        {foundations.map((component) => (
+          <ComponentCard key={component.slug} component={component} />
+        ))}
+      </div>
+      {/* verwsion details and totoal components count */}
+      <div className="mt-6 flex items-center justify-end">
+        <p className="text-muted-foreground text-sm">
+          Version: {registry.version.version} | Total foundations:{" "}
+          {foundations.length} | Last updated: {registry.version.lastUpdated}
+        </p>
+      </div>
+    </Container>
+  );
+}
+
