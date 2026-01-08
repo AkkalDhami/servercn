7e1117033ffb07d6f3e873bda02afad751e25074
AkkalDhami <dhamiakkal21@gmail.com>
2026-01-08 09:03:30 +0545

    Enhance layout and styling with smooth scrolling behavior and improved code display properties for better user experience.

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className={fontVariables}>
       <body
-        className={`selection:bg-primary selection:text-primary-foreground mx-auto max-w-387.5 antialiased`}
+        className={`selection:bg-primary selection:text-primary-foreground mx-auto max-w-387.5 scroll-smooth antialiased`}
       >
         <ThemeProvider
           attribute="class"
