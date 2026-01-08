ff5f4485f5beb368cc42ced27153e2dc38eae31b
AkkalDhami <dhamiakkal21@gmail.com>
2026-01-08 09:04:48 +0545

    Refactor DocsSidebar and Navbar components: Implement dynamic item grouping using getTypeItems, enhance navigation styling, and improve type safety with Route integration for better user experience and maintainability.

--- apps/web/src/components/layouts/docs-sidebar.tsx
+++ apps/web/src/components/layouts/docs-sidebar.tsx
@@ -6,58 +6,52 @@ import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import SearchCommand from "../command/search-command";
+import { Route } from "next";
+import { getTypeItems } from "@/lib/source";
 
-const mapped = [...new Set(registry.items.map((item) => item.type))];
+export const ITEM_GROUP_NAMING = {
+  guide: "Getting Started",
+  component: "Components",
+  blueprint: "Blueprints",
+  foundation: "Foundations",
+  model: "Models",
+};
 
 const navSections = [
   {
-    title: "Getting Started",
-    items: [
-      {
-        title: "Introduction",
-        url: "/docs/introduction",
-      },
-      {
-        title: "Installation",
-        url: "/docs/installation",
-      },
-      {
-        title: "Project Structure",
-        url: "/docs/project-structure",
-      },
-    ],
-  },
-  { 
-    title: `${mapped[0]}s`,
-    items: registry.items
-      .sort((a, b) => a.title.localeCompare(b.title))
-      .filter((item) => item.type == "component")
-      .map((item) => ({
-        title: item.title,
-        url: item.docs,
-      })),
+    title: ITEM_GROUP_NAMING.guide,
+    items: getTypeItems("guide"),
   },
   {
-    title: `${mapped[1]}s`,
-    items: registry.items
-      .sort((a, b) => a.title.localeCompare(b.title))
-      .filter((item) => item.type == "blueprint")
-      .map((item) => ({
-        title: item.title,
-        url: item.docs,
-      })),
+    title: ITEM_GROUP_NAMING.foundation,
+    items: getTypeItems("foundation"),
+  },
+  {
+    title: ITEM_GROUP_NAMING.component,
+    items: getTypeItems("component"),
+  },
+  {
+    title: ITEM_GROUP_NAMING.blueprint,
+    items: getTypeItems("blueprint"),
   },
 
+  {
+    title: ITEM_GROUP_NAMING.model,
+    items: getTypeItems("model"),
+  },
 ];
 
 export default function DocsSidebar() {
   const pathname = usePathname();
 
   return (
-    <nav className="sidebar sticky top-18 left-0 z-10 h-full max-h-[calc(100vh-6rem)] w-full space-y-6 overflow-y-auto px-3">
+    <nav className="no-scrollbar sticky top-18 left-0 z-10 h-full max-h-[calc(100vh-6rem)] w-full space-y-6 overflow-y-auto px-3">
       <SearchCommand />
       {navSections.map((section) => (
-        <div key={section.title}>
+        <div
+          key={section.title}
+          className={cn(section.items.length > 0 ? "" : "hidden")}
+        >
           <h3 className="relative z-10 w-11/12 pb-4 text-sm/6 font-[450] uppercase">
             {section.title}
           </h3>
@@ -68,9 +62,9 @@ export default function DocsSidebar() {
               return (
                 <li key={item.url}>
                   <Link
-                    href={item.url}
+                    href={item.url as Route}
                     className={cn(
-                      "relative inline-flex items-center pl-4 text-base font-medium",
+                      "relative inline-flex w-full items-center pl-4 text-base font-medium",
                       active
                         ? "text-accent-foreground font-medium"
                         : "text-muted-foreground hover:text-foreground",
@@ -80,6 +74,9 @@ export default function DocsSidebar() {
                       <div className="bg-primary absolute top-0 -left-px z-1 h-full w-px" />
                     )}
                     <span className="">{item.title}</span>
+                    {item.status === "unstable" && (
+                      <div className="absolute top-1/2 right-0 z-1 size-2 -translate-y-1/2 rounded-full bg-yellow-500" />
+                    )}
                   </Link>
                 </li>
               );
--- apps/web/src/components/layouts/navbar.tsx
+++ apps/web/src/components/layouts/navbar.tsx
@@ -2,13 +2,17 @@
 import { Button } from "../ui/button";
 import Logo from "./logo";
 import ThemeToggle from "./theme-toggle";
-import { Kbd } from "@/components/ui/kbd";
 import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
 import { siteConfig } from "@/lib/config";
 import SearchCommand from "../command/search-command";
+import { Route } from "next";
+import { cn } from "@/lib/utils";
+import {  usePathname } from "next/navigation";
 const links = siteConfig.navItems;
 export default function Navbar() {
+  const path = usePathname();
+  console.log(path);
   return (
     <header className="bg-background fixed top-0 left-0 z-40 w-full">
       <nav className="mx-auto flex max-w-368 items-center justify-between px-4 py-2 md:py-3">
@@ -17,8 +21,8 @@ export default function Navbar() {
           {links.map((link) => (
             <li key={link.href}>
               <Link
-                href={link.href}
-                className="text-muted-foreground hover:text-foreground"
+                href={link.href as Route}
+                className={cn("text-muted-foreground hover:text-foreground", path.includes(link.href) && "text-foreground")}
               >
                 {link.label}
               </Link>
@@ -27,7 +31,7 @@ export default function Navbar() {
         </ul>
         <div className="flex items-center gap-3">
           <SearchCommand />
-         
+
           <ThemeToggle />
           <Button asChild size={"icon"} variant={"secondary"}>
             <Link href="https://github.com/akkaldhami" target="_blank">
