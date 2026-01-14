6fef073c10ccd18e372fae290a71b9929bf679f0
AkkalDhami <dhamiakkal21@gmail.com>
2026-01-14 11:25:32 +0545

    refactor: standardize formatting and structure in async-handler files

--- packages/templates/components/async-handler/express/mvc/src/utils/async-handler.ts
+++ packages/templates/components/async-handler/express/mvc/src/utils/async-handler.ts
@@ -1,10 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
-export type AsyncRouteHandler = (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => Promise<unknown>;
+export type AsyncRouteHandler = (req: Request, res: Response, next: NextFunction) => Promise<unknown>;
 
 export function AsyncHandler(fn: AsyncRouteHandler) {
   return function (req: Request, res: Response, next: NextFunction) {
