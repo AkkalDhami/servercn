dcdcf9cc8b3830c8158b5533d091b0dba881212a
AkkalDhami <dhamiakkal21@gmail.com>
2026-01-22 06:43:42 +0545

    feat: Add commitlint configurations, auth schema, JWT utils, and Express rate limiting middleware.

--- packages/templates/components/rate-limiter/express/mvc/src/middlewares/rate-limiter.ts
+++ packages/templates/components/rate-limiter/express/mvc/src/middlewares/rate-limiter.ts
@@ -6,6 +6,7 @@ import { STATUS_CODES } from "../constants/status-codes";
  * Standard rate limiter middleware
  * Limits each IP to 100 requests per 15-minute window
  */
+
 export const rateLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per window
@@ -35,6 +36,96 @@ export const authRateLimiter = rateLimit({
       )
     );
   }
+});
+
+/**
+ * Rate limiter for login route
+ */
+export const signinRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: {
+    success: false,
+    message: "Too many login attempts, please try again later.",
+    statusCode: 429
+  },
+  standardHeaders: true,
+  legacyHeaders: false
+});
+
+/**
+ * Rate limiter for registration route
+ */
+export const signupRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: {
+    success: false,
+    message: "Too many registration attempts, please try again later.",
+    statusCode: 429
+  },
+  standardHeaders: true,
+  legacyHeaders: false
+});
+
+export const otpRequestLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 6,
+  message: {
+    success: false,
+    message: "Too many OTP requests. Please try again later.",
+    statusCode: 429
+  },
+  standardHeaders: true,
+  legacyHeaders: false
+});
+
+export const otpVerificationLimiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  max: 6,
+  message: {
+    success: false,
+    message: "Too many OTP verification attempts. Please try again later.",
+    statusCode: 429
+  },
+  standardHeaders: true,
+  legacyHeaders: false
+});
+
+export const resetPasswordLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 6,
+  message: {
+    success: false,
+    message: "Too many password reset attempts, please try again later.",
+    statusCode: 429
+  },
+  standardHeaders: true,
+  legacyHeaders: false
+});
+
+export const deleteAccountLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: {
+    success: false,
+    message: "Too many account deletion attempts, please try again later.",
+    statusCode: 429
+  },
+  standardHeaders: true,
+  legacyHeaders: false
+});
+
+export const changePasswordLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 5,
+  message: {
+    success: false,
+    message: "Too many password change attempts, please try again later.",
+    statusCode: 429
+  },
+  standardHeaders: true,
+  legacyHeaders: false
 });
 
 /**
