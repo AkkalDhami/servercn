ae9d0a33519df28031309197b82165c92b84b563
AkkalDhami <dhamiakkal21@gmail.com>
2026-01-29 16:42:19 +0545

    refactor: schema structure and update config references
    Moved user schema files into a dedicated schemas directory and created index.ts files to export schemas. Updated drizzle.config.ts files to reference the new index.ts entry point for schemas and added type safety with Config. This improves organization and scalability for schema management.

--- packages/cli/templates/foundations/drizzle-pg-starter/mvc/drizzle.config.ts
+++ packages/cli/templates/foundations/drizzle-pg-starter/mvc/drizzle.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from "drizzle-kit";
+import { Config, defineConfig } from "drizzle-kit";
 
 import env from "./src/configs/env";
 
 export default defineConfig({
   out: "./src/drizzle/migrations",
-  schema: "./src/drizzle/schema.ts",
+  schema: "./src/drizzle/index.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: env.DATABASE_URL!
   },
   verbose: true,
   strict: true
-});
+}) satisfies Config;
