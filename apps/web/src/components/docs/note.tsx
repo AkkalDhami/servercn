5f8dfc4cb18d45754aeb96d7a9d1274918671c29
AkkalDhami <dhamiakkal21@gmail.com>
2026-02-11 12:21:05 +0545

    refactor: update note component styling and correct swagger configuration references
    Modified the Note component's styling to use a blue background and border for better visibility. Updated references in the documentation to reflect the new swagger configuration file name, changing instances of  to  for consistency.

--- apps/web/src/components/docs/note.tsx
+++ apps/web/src/components/docs/note.tsx
@@ -1,6 +1,6 @@
 export default function Note({ text }: { text: string }) {
   return (
-    <div className="bg-muted xsm:max-w[360px] text-muted-primary my-3 flex max-w-[320px] gap-2 rounded-md border border-neutral-300 px-2 py-2 md:max-w-200 dark:border-neutral-900">
+    <div className="xsm:max-w[360px] my-3 flex max-w-[320px] gap-2 rounded-md border border-blue-300 bg-blue-500/10 px-2 py-2 text-blue-600 md:max-w-200 dark:border-blue-900">
       <p className="text-sm leading-relaxed sm:text-base">{text}</p>
     </div>
   );
