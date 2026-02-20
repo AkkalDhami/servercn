adaa4f72eb9c2c685ace8e934231a7684bb089d1
AkkalDhami <dhamiakkal21@gmail.com>
2026-02-20 14:49:28 +0545

    refactor(templates): update templates schema, registry

--- packages/templates/components/verify-auth-middleware/express/mvc/src/types/user.ts
+++
@@ -1,8 +0,0 @@
-import { Request } from "express";
-import mongoose from "mongoose";
-
-export interface UserRequest extends Request {
-  user?: {
-    _id?: string | mongoose.Types.ObjectId;
-  };
-}
