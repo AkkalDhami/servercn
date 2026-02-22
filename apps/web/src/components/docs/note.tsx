ae7e240a535fbf857d8e94556baa5a381c7057fc
AkkalDhami <dhamiakkal21@gmail.com>
2026-02-22 11:21:09 +0545

    fix(docs): update padding in Note and Warning components for consistency

--- apps/web/src/components/docs/note.tsx
+++ apps/web/src/components/docs/note.tsx
@@ -1,6 +1,6 @@
 export default function Note({ text }: { text: string }) {
   return (
-    <div className="xsm:max-w[360px] my-3 flex max-w-[320px] gap-2 border-l-4 text border-blue-500 bg-blue-500/10 sm:pl-3 sm:py-2.5 px-2 py-2 text-blue-500 md:max-w-200">
+    <div className="xsm:max-w[360px] my-3 flex max-w-[320px] gap-2 border-l-4 text border-blue-500 bg-blue-500/10 sm:py-2.5 px-3 py-2 text-blue-500 md:max-w-200">
       <p className="leading-relaxed tracking-wide font-medium text-base">{text}</p>
     </div>
   );
--- apps/web/src/components/docs/warning.tsx
+++ apps/web/src/components/docs/warning.tsx
@@ -1,6 +1,6 @@
 export default function Warning({ text }: { text: string }) {
   return (
-    <div className="xsm:max-w[360px] text my-3 flex max-w-[320px] gap-2 border-l-4 border-yellow-500 bg-yellow-500/10 px-2 py-2 text-yellow-600 sm:py-2.5 sm:pl-3 md:max-w-200">
+    <div className="xsm:max-w[360px] text my-3 flex max-w-[320px] gap-2 border-l-4 border-yellow-500 bg-yellow-500/10 px-3 py-2 text-yellow-600 sm:py-2.5 md:max-w-200">
       <p className="text-base leading-relaxed font-medium tracking-wide">
         {text}
       </p>
