bf2947bc7469f22b0ec87565dc6252d28da1f03e
AkkalDhami <dhamiakkal21@gmail.com>
2026-04-02 11:17:56 +0545

    feat: add MongoDB and MySQL schemas and configurations for Todo application
    - Introduced MongoDB model for Todo with appropriate fields, validations, and performance indexes.
    - Created MySQL schema for Todo, including configuration and timestamp management.
    - Developed user schema for MySQL with relationships to Todo and necessary fields.
    - Added configuration files for both MongoDB and MySQL to support database operations.

---
+++ packages/templates/node/nextjs/schema/todo/todo/mysql/drizzle/file-api/src/db/schemas/todo.schema.ts
@@ -0,0 +1,38 @@
+import {
+  boolean,
+  mysqlTable,
+  text,
+  varchar,
+  index,
+  int
+} from "drizzle-orm/mysql-core";
+import { relations } from "drizzle-orm";
+import { timestamps } from "./schema.helper";
+
+export const todos = mysqlTable(
+  "todos",
+  {
+    id: int("id").primaryKey().autoincrement(),
+    userId: int("user_id")
+      // .references(() => users.id, { onDelete: "cascade" }),
+      .notNull(),
+
+    title: varchar("title", { length: 255 }).notNull(),
+    description: text("description"),
+    completed: boolean("completed").notNull().default(false),
+
+    ...timestamps
+  },
+  table => [
+    index("todo_user_id_idx").on(table.userId),
+    index("todo_completed_idx").on(table.completed)
+  ]
+);
+
+//TODO: Relations between
+//? ii. todo and users.
+//? (many-to-one)
+
+
+export type Todo = typeof todos.$inferSelect;
+export type NewTodo = typeof todos.$inferInsert;
