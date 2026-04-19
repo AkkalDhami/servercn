da0c8e1e4ff5e24dbb80bf9861627f6d702fb6ca
AkkalDhami <dhamiakkal21@gmail.com>
2026-04-19 21:09:31 +0545

    refactor(docs): enhance styling for CopyButton, Note, and Warning components
    Updated the CopyButton component to include a hover background color. Modified the Note and Warning components to use rounded borders and adjusted border colors for improved visual consistency. These changes enhance the user interface and maintain a cohesive design across documentation components.

--- apps/web/src/components/docs/note.tsx
+++ apps/web/src/components/docs/note.tsx
@@ -1,6 +1,6 @@
 export default function Note({ text }: { text: string }) {
   return (
-    <div className="xsm:max-w[360px] text my-3 flex max-w-[320px] gap-2 border-l-4 border-blue-500 bg-blue-500/10 px-3 py-2 text-blue-500 sm:py-2.5 md:max-w-200">
+    <div className="xsm:max-w[360px] text my-3 flex max-w-[320px] gap-2 rounded-lg border border-blue-300 bg-blue-500/10 px-3 py-2 text-blue-500 sm:py-2.5 md:max-w-200 dark:border-blue-800">
       <p className="text-base leading-relaxed font-medium tracking-wide">
         {text}
       </p>
