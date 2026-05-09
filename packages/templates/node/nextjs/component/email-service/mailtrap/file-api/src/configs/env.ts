45b23c2ae099c89046a94c99a799e65041c5d0a8
AkkalDhami <dhamiakkal21@gmail.com>
2026-05-09 10:23:22 +0545

    feat(email-service): add Mailtrap configuration and environment validation
    - Introduced Mailtrap client configuration with API key from environment variables.
    - Added environment schema validation using Zod to ensure required variables are set.
    - Implemented error handling for invalid environment configurations.

---
+++ packages/templates/node/nextjs/component/email-service/mailtrap/file-api/src/configs/env.ts
@@ -0,0 +1,20 @@
+import z from "zod";
+
+export const envSchema = z.object({
+  MAILTRAP_API_KEY: z.string(),
+  EMAIL_FROM: z.email()
+});
+
+export type Env = z.infer<typeof envSchema>;
+
+const result = envSchema.safeParse(process.env);
+
+if (!result.success) {
+  console.error("❌ Invalid environment configuration");
+  console.error(z.prettifyError(result.error));
+  process.exit(1);
+}
+
+export const env: Readonly<Env> = Object.freeze(result.data);
+
+export default env;
