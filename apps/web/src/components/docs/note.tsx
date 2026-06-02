8f4ff917e3ef1638c8c9c9e38bcbade71b95a264
AkkalDhami <dhamiakkal21@gmail.com>
2026-06-02 08:13:01 +0545

    feat: implement interactive file viewer component with code highlighting and file tree navigation

--- apps/web/src/components/docs/note.tsx
+++ apps/web/src/components/docs/note.tsx
@@ -1,6 +1,6 @@
 export default function Note({ text }: { text: string }) {
   return (
-    <div className="text my-3 flex gap-2 rounded-lg border border-blue-300 bg-blue-500/10 px-3 py-2 text-blue-500 sm:py-2.5 dark:border-blue-950">
+    <div className="text max-w-code my-3 flex gap-2 rounded-lg border border-blue-300 bg-blue-500/10 px-3 py-2 text-blue-500 sm:py-2.5 dark:border-blue-950">
       <p className="text-base leading-relaxed font-medium tracking-wide">
         {text}
       </p>
