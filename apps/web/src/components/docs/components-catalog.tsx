f5920c77e11c3084972a538565f7955b7adb1851
AkkalDhami <dhamiakkal21@gmail.com>
2026-06-25 07:06:38 +0545

    feat(registry mongodb-mongoose): add nextjs support
    add nextjs framework configuration and associated template files

--- apps/web/src/components/docs/components-catalog.tsx
+++ apps/web/src/components/docs/components-catalog.tsx
@@ -19,7 +19,7 @@ export function ComponentsCatalog({ type }: { type: ItemType }) {
     <>
       <div className="screen-line-after &>*]:border grid divide-x sm:grid-cols-2 lg:grid-cols-3 [&>*:nth-child(3n)]:border-r-0 [&>*:nth-child(3n+1)]:border-l-0">
         {components
-          .filter(c => c.status !== "experimental")
+          .filter(c => !["experimental", "deprecated"].includes(c.status))
           .map(component =>
             ["blueprint", "schema"].includes(type) ? (
               <ItemWithDBCard key={component.slug} item={component} />
