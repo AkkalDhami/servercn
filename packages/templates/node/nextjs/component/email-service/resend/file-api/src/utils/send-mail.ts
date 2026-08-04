9610bf525baf4eac7cf9bc3a8ef63b507a2ca065
Reuben Santoso <reuben.santoso@gmail.com>
2026-08-03 18:32:47 -0700

    fix(templates): surface Resend send errors instead of silently succeeding

--- packages/templates/node/nextjs/component/email-service/resend/file-api/src/utils/send-mail.ts
+++ packages/templates/node/nextjs/component/email-service/resend/file-api/src/utils/send-mail.ts
@@ -10,13 +10,19 @@ type sendMail = {
 };
 
 export async function sendEmail({ from, email, subject, html }: sendMail) {
-  return await resend.emails.send({
+  const response = await resend.emails.send({
     from: from || `<${env.EMAIL_FROM}>`,
     to: email,
     subject,
     replyTo: email,
     html
   });
+
+  if (response.error) {
+    throw new Error(response.error.message || "Failed to send email");
+  }
+
+  return response.data;
 }
 
 /**
